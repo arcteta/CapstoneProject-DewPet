6f7accdda24cf32fbc7b0d690c6efdddfa93aff1
Luthfi Yafi Alfiansyah <luthfialghz@MacBook-Air-Luthfi.local>
2022-05-19 11:41:04 +0700

    Pembuatan Splash Screen dan Onboarding Page

---
+++ app/src/main/java/com/bangkit/dewpet/OnboardingActivity.kt
@@ -0,0 +1,13 @@
+package com.bangkit.dewpet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+
+        supportActionBar?.hide()
+    }
+}
